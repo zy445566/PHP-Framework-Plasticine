c3c28202224c8312c2a9c5bd5d21f51bfe6a36bf
ZhangYuan <zyct@vip.qq.com>
2017-09-21 22:52:53 -0500

    del js and css

--- Plasticine/view/header.php
+++ Plasticine/view/header.php
@@ -8,10 +8,5 @@
 	<script src="{{url("js")}}/respond.min.js"></script>
 	<![endif]-->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css">
-	<script type="text/javascript" src="/js/jquery.min.js"></script>
-	<script type="text/javascript" src="/js/respond.min.js"></script>
-	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
 </head>
 <body>
